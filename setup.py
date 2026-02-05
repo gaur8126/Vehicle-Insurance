f86c65d002576b3f97d86cd495d5b7db3baa379b
gaur8126 <gaurlokesh1211@gmail.com>
2026-02-05 21:46:55 +0530

    project setup is ready

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Lokesh Gaur",
+    author_email= "gaurlokesh1211@gmail.com",
+    packages=find_packages()
+
+)
