3eeed3c073cf4681d679b5bdee5d5614fcfecc91
gaur8126 <gaurlokesh1211@gmail.com>
2026-02-19 14:31:14 +0530

    environment variables set

--- src/configuration/aws_connection.py
+++ src/configuration/aws_connection.py
@@ -1,5 +1,7 @@
 import boto3
 import os 
+from dotenv import load_dotenv
+load_dotenv()
 from src.constants import AWS_ACCESS_KEY_ID_ENV_KEY, AWS_SECRET_ACCESS_KEY_ENV_KEY, REGION_NAME
 
 
@@ -17,8 +19,8 @@ class S3Client:
         """
 
         if S3Client.s3_resource == None or S3Client.s3_client == None:
-            __access_key_id = os.getenv("AWS_ACCESS_KEY_ID_ENV_KEY")
-            __secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY_ENV_KEY")
+            __access_key_id = os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY)
+            __secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY)
             if __access_key_id is None:
                 raise Exception(f"Environment variable: {AWS_ACCESS_KEY_ID_ENV_KEY} is not set")
             if __secret_access_key is None:
